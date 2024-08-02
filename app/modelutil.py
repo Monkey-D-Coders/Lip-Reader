a7d8b712380272e9656aeb73687639824d1665f6
Omm Prakash Sahoo <69079655+Antoniocolapso@users.noreply.github.com>
2024-08-02 21:17:19 +0530

    Update modelutil.py

--- app/modelutil.py
+++ app/modelutil.py
@@ -27,6 +27,6 @@ def load_model() -> Sequential:
 
     model.add(Dense(41, kernel_initializer='he_normal', activation='softmax'))
 
-    model.load_weights(os.path.join('..','models','checkpoint'))
+    model.load_weights(os.path.join('..','models','checkpoint.keras'))
 
     return model
